f1939821dd8180e4c23cedbf6623694abd3527f2
Dimasariza <roganda.sihombing11@gmail.com>
2023-05-24 19:19:04 +0700

    Auto-generated commit [ci skip]

--- common.js
+++ common.js
@@ -11,12 +11,13 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "TopBarModule": () => (/* binding */ TopBarModule)
 /* harmony export */ });
-/* harmony import */ var _angular_common__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @angular/common */ 36362);
+/* harmony import */ var _angular_common__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @angular/common */ 36362);
 /* harmony import */ var _theme_theme_module__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../@theme/theme.module */ 80268);
 /* harmony import */ var _top_bar_component__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./top-bar.component */ 78713);
-/* harmony import */ var _nebular_theme__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @nebular/theme */ 68253);
-/* harmony import */ var _techiediaries_ngx_qrcode__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! @techiediaries/ngx-qrcode */ 33244);
-/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/core */ 3184);
+/* harmony import */ var _nebular_theme__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! @nebular/theme */ 68253);
+/* harmony import */ var _techiediaries_ngx_qrcode__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! @techiediaries/ngx-qrcode */ 33244);
+/* harmony import */ var _qr_code_qr_code_dialog__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./qr-code/qr-code.dialog */ 5198);
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @angular/core */ 3184);
 
 
 
@@ -24,28 +25,29 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
-// import { PrintQRCodeComponent } from "./qr-code/qr-code.dialog";
+
 class TopBarModule {
 }
 TopBarModule.ɵfac = function TopBarModule_Factory(t) { return new (t || TopBarModule)(); };
-TopBarModule.ɵmod = /*@__PURE__*/ _angular_core__WEBPACK_IMPORTED_MODULE_2__["ɵɵdefineNgModule"]({ type: TopBarModule });
-TopBarModule.ɵinj = /*@__PURE__*/ _angular_core__WEBPACK_IMPORTED_MODULE_2__["ɵɵdefineInjector"]({ imports: [[
-            _angular_common__WEBPACK_IMPORTED_MODULE_3__.CommonModule,
+TopBarModule.ɵmod = /*@__PURE__*/ _angular_core__WEBPACK_IMPORTED_MODULE_3__["ɵɵdefineNgModule"]({ type: TopBarModule });
+TopBarModule.ɵinj = /*@__PURE__*/ _angular_core__WEBPACK_IMPORTED_MODULE_3__["ɵɵdefineInjector"]({ imports: [[
+            _angular_common__WEBPACK_IMPORTED_MODULE_4__.CommonModule,
             _theme_theme_module__WEBPACK_IMPORTED_MODULE_0__.ThemeModule,
-            _nebular_theme__WEBPACK_IMPORTED_MODULE_4__.NbCardModule,
-            _nebular_theme__WEBPACK_IMPORTED_MODULE_4__.NbButtonModule,
-            _nebular_theme__WEBPACK_IMPORTED_MODULE_4__.NbIconModule,
-            _techiediaries_ngx_qrcode__WEBPACK_IMPORTED_MODULE_5__.NgxQRCodeModule,
-            _nebular_theme__WEBPACK_IMPORTED_MODULE_4__.NbDialogModule.forChild({
+            _nebular_theme__WEBPACK_IMPORTED_MODULE_5__.NbCardModule,
+            _nebular_theme__WEBPACK_IMPORTED_MODULE_5__.NbButtonModule,
+            _nebular_theme__WEBPACK_IMPORTED_MODULE_5__.NbIconModule,
+            _techiediaries_ngx_qrcode__WEBPACK_IMPORTED_MODULE_6__.NgxQRCodeModule,
+            _nebular_theme__WEBPACK_IMPORTED_MODULE_5__.NbDialogModule.forChild({
                 closeOnBackdropClick: true
             })
         ]] });
-(function () { (typeof ngJitMode === "undefined" || ngJitMode) && _angular_core__WEBPACK_IMPORTED_MODULE_2__["ɵɵsetNgModuleScope"](TopBarModule, { declarations: [_top_bar_component__WEBPACK_IMPORTED_MODULE_1__.TopBarComponent], imports: [_angular_common__WEBPACK_IMPORTED_MODULE_3__.CommonModule,
+(function () { (typeof ngJitMode === "undefined" || ngJitMode) && _angular_core__WEBPACK_IMPORTED_MODULE_3__["ɵɵsetNgModuleScope"](TopBarModule, { declarations: [_top_bar_component__WEBPACK_IMPORTED_MODULE_1__.TopBarComponent,
+        _qr_code_qr_code_dialog__WEBPACK_IMPORTED_MODULE_2__.PrintQRCodeComponent], imports: [_angular_common__WEBPACK_IMPORTED_MODULE_4__.CommonModule,
         _theme_theme_module__WEBPACK_IMPORTED_MODULE_0__.ThemeModule,
-        _nebular_theme__WEBPACK_IMPORTED_MODULE_4__.NbCardModule,
-        _nebular_theme__WEBPACK_IMPORTED_MODULE_4__.NbButtonModule,
-        _nebular_theme__WEBPACK_IMPORTED_MODULE_4__.NbIconModule,
-        _techiediaries_ngx_qrcode__WEBPACK_IMPORTED_MODULE_5__.NgxQRCodeModule, _nebular_theme__WEBPACK_IMPORTED_MODULE_4__.NbDialogModule], exports: [_top_bar_component__WEBPACK_IMPORTED_MODULE_1__.TopBarComponent] }); })();
+        _nebular_theme__WEBPACK_IMPORTED_MODULE_5__.NbCardModule,
+        _nebular_theme__WEBPACK_IMPORTED_MODULE_5__.NbButtonModule,
+        _nebular_theme__WEBPACK_IMPORTED_MODULE_5__.NbIconModule,
+        _techiediaries_ngx_qrcode__WEBPACK_IMPORTED_MODULE_6__.NgxQRCodeModule, _nebular_theme__WEBPACK_IMPORTED_MODULE_5__.NbDialogModule], exports: [_top_bar_component__WEBPACK_IMPORTED_MODULE_1__.TopBarComponent] }); })();
 
 
 /***/ }),
