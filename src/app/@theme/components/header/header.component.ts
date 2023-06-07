60b9dced9a1c3bacb4ad1e90f8dce2c04bfaaf35
Dimasariza <roganda.sihombing11@gmail.com>
2023-06-08 06:51:37 +0700

    add circuit menu

--- src/app/@theme/components/header/header.component.ts
+++ src/app/@theme/components/header/header.component.ts
@@ -46,7 +46,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
-    this.user = { name : 'David', picture : '../../../../assets/image/profile.jpg' }
+    this.user = { name : 'David', picture : 'https://th.bing.com/th/id/R.1897f97542abc7ed782d257ee781bc40?rik=xFlJ%2bhoKSCg5cA&riu=http%3a%2f%2fpluspng.com%2fimg-png%2fuser-png-icon-male-user-icon-512.png&ehk=pivQjzqNrQFIa6FjMTnQevAdjpeQ4Sh3OXEgbMFIE2o%3d&risl=&pid=ImgRaw&r=0' }
     this.currentTheme = this.themeService.currentTheme;
 
     const { xl } = this.breakpointService.getBreakpointsMap();
