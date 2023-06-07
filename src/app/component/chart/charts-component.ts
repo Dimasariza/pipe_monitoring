9cae48277c8cbc0cd9353dc3c4ce0b4b1e7ee962
Dimasariza <roganda.sihombing11@gmail.com>
2023-06-07 08:01:06 +0700

    add bar chart

--- src/app/component/chart/charts-component.ts
+++ src/app/component/chart/charts-component.ts
@@ -6,8 +6,59 @@ Chart.register(...registerables);
     template : ""
 })
 export class ChartsComponent implements OnInit {
-    constructor() { }
-    defineOptions(chartTitle, yTitle, xTitle) {
+    constructor() {
+
+    }
+
+    public chartType :any = 'line';
+    public datasets : any = [
+        {
+            label: "CML 1A",
+            data: ['467', '576', '572', '79', '92'],
+            backgroundColor: 'transparent',
+            borderColor: 'rgba(255,0,0,.5)',
+        },
+        {
+            label: "CML 1B",
+            data: ['542', '542', '536', '327', '17'],
+            backgroundColor: 'transparent',
+            borderColor: 'rgba(100,100,0,.5)',
+        },
+        {
+            label: "CML 1C",
+            data: ['504', '142', '336', '317', '100'],
+            backgroundColor: 'transparent',
+            borderColor: 'rgba(10,50,100,.5)',
+        },
+        {
+            label: "CML 1D",
+            data: ['52', '42', '336', '527', '517'],
+            backgroundColor: 'transparent',
+            borderColor: 'rgba(90,190,90,.5)',
+        },
+        {
+            label: "CML 1E",
+            data: ['50', '402', '336', '427', '217'],
+            backgroundColor: 'transparent',
+            borderColor: 'rgba(190,190,10,.5)',
+        },
+        {
+            label: "CML 1F",
+            data: ['92', '342', '536', '227', '117'],
+            backgroundColor: 'transparent',
+            borderColor: 'rgba(200,200,20,.5)',
+        },
+        {
+            label: "CML 1G",
+            data: ['52', '242', '436', '327', '217'],
+            backgroundColor: 'transparent',
+            borderColor: 'rgba(20,150,200,.5)',
+        }  
+    ]
+
+    public yLabels : any[] = ['2020', '2021', '2022', '2023', '2024']
+
+    defineOptions({chartTitle = '', yTitle = '', xTitle = ''}) {
         this.options = {
             responsive: true,
             plugins: {
@@ -52,8 +103,12 @@ export class ChartsComponent implements OnInit {
     }
 
     generateChart(id) {
+        this.data = {
+            labels: this.yLabels, 
+            datasets: this.datasets
+        }
         this.chart = new Chart(id, {
-            type: 'line',
+            type: this.chartType,
             data: this.data,
             options : this.options
         });
@@ -61,53 +116,7 @@ export class ChartsComponent implements OnInit {
 
     public options : any;
     public chart;
-    public data : any = {
-        labels: ['2020', '2021', '2022', '2023', '2024'], 
-        datasets: [
-            {
-                label: "CML 1A",
-                data: ['467','576', '572', '79', '92'],
-                backgroundColor: 'transparent',
-                borderColor: 'rgba(255,0,0,.5)',
-            },
-            {
-                label: "CML 1B",
-                data: ['542', '542', '536', '327', '17'],
-                backgroundColor: 'transparent',
-                borderColor: 'rgba(100,100,0,.5)',
-            },
-            {
-                label: "CML 1C",
-                data: ['504', '142', '336', '317', '100'],
-                backgroundColor: 'transparent',
-                borderColor: 'rgba(10,50,100,.5)',
-            },
-            {
-                label: "CML 1D",
-                data: ['52', '42', '336', '527', '517'],
-                backgroundColor: 'transparent',
-                borderColor: 'rgba(90,190,90,.5)',
-            },
-            {
-                label: "CML 1E",
-                data: ['50', '402', '336', '427', '217'],
-                backgroundColor: 'transparent',
-                borderColor: 'rgba(190,190,10,.5)',
-            },
-            {
-                label: "CML 1F",
-                data: ['92', '342', '536', '227', '117'],
-                backgroundColor: 'transparent',
-                borderColor: 'rgba(200,200,20,.5)',
-            },
-            {
-                label: "CML 1G",
-                data: ['52', '242', '436', '327', '217'],
-                backgroundColor: 'transparent',
-                borderColor: 'rgba(20,150,200,.5)',
-            }  
-        ]
-    }
+    public data : any 
 
     ngOnInit(): void {  }
 }
